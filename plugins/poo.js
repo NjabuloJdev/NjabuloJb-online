1f11480d9bdf4f5ea12626e2ee5c22fa67858713
NjabuloJb <fanajb65@gmail.com>
2025-08-24 02:38:43 +0200

    Create poo.js

---
+++ plugins/poo.js
@@ -0,0 +1,44 @@
+import config from '../config.cjs';
+import pkg, { prepareWAMessageMedia } from "@whiskeysockets/baileys";
+const { generateWAMessageFromContent, proto } = pkg;
+
+const ping = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+
+  if (cmd === "pingo") {
+    const start = new Date().getTime();
+
+    const reactionEmojis = ['🔥', '⚡', '🚀', '💨', '🎯', '🎉', '🌟', '💥', '🕐', '🔹'];
+    const textEmojis = ['💎', '🏆', '⚡️', '🚀', '🎶', '🌠', '🌀', '🔱', '🛡️', '✨'];
+
+    const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
+    let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+
+    // Ensure reaction and text emojis are different
+    while (textEmoji === reactionEmoji) {
+      textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    }
+
+    await m.React(textEmoji);
+
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*JINX-XMD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+
+    const button = {
+      "buttonText": "Follow our Channel",
+      "buttonUrl": "https://whatsapp.com/channel/0029VagJlnG6xCSU2tS1Vz19"
+    };
+
+    await Matrix.sendMessage(m.from, {
+      text: text,
+      footer: '',
+      buttons: [button],
+      headerType: 1
+    }, { quoted: m });
+  }
+};
+
+export default ping;
