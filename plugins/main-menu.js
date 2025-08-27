a7d531609a21635ffe3f8ab7afc1b02771cae733
NjabuloJb <fanajb65@gmail.com>
2025-08-27 15:44:02 +0200

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,7 +1,7 @@
 import moment from "moment-timezone";
 import fs from "fs";
 import os from "os";
-import pkg from "baileys-pro";
+import pkg, { prepareWAMessageMedia } from "@whiskeysockets/baileys";
 const { generateWAMessageFromContent, proto } = pkg;
 import config from "../config.cjs";
 import axios from "axios";
